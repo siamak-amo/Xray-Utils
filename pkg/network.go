c1d1ee4ff2c3db10594d5e675dd7c2cc7ba89690
siamak a.m.o <edu.siamak@gmail.com>
2025-12-06 12:51:01 +0330

    Implement network

---
+++ pkg/network.go
@@ -0,0 +1,88 @@
+package pkg
+
+import (
+	"fmt"
+	v4 "github.com/v2fly/v2ray-core/v5/infra/conf/v4"
+)
+
+func set_stream_settings(args URLmap, dst *v4.StreamConfig) (e error) {
+	switch (args[Network]) {
+	case "ws":
+		e = unmarshal_H (&dst.WSSettings,
+			fmt.Sprintf (`{"path": "%s", "headers": {"Host": "%s"}}`,
+				args[WS_Path], args[WS_Headers]),
+		);
+		break
+
+	case "tcp":
+		switch (args[TCP_HeaderType]) {
+		case "none", "":
+			e = unmarshal_H (&dst.TCPSettings, (`{"header": {"type": "none"}}`));
+			break;
+
+		case "http":
+			e = unmarshal_H (&dst.TCPSettings, fmt.Sprintf (
+				`{
+                    "header": {"type": "%s"}, "request": {
+                      "version": "1.1", "path": ["%s"], "headers": {"Host": "%s"}
+                    }
+                 }`,
+				args[TCP_HeaderType], args[TCP_HTTP_Path], args[TCP_HTTP_Host]),
+			);
+			break;
+
+		default:
+			break;
+		} // End of `switch (args[TCP_HeaderType])`
+		break
+
+	default:
+		return not_implemented ("network " + args[Network])
+	}
+	if nil != e {
+		// log
+	}
+
+	switch (args[Security]) {
+	case "", "none":
+		break
+
+	case "tls":
+		map_normal (args, TLS_AllowInsecure, "true")
+		if e = unmarshal_H (&dst.TLSSettings,
+			fmt.Sprintf (
+				`{"servername": "%s", "allowInsecure": %s, "alpn": ["h2", "http/1.1"]}`,
+				args[TLS_sni], args[TLS_AllowInsecure],
+			),
+		); e != nil {
+			// log
+		}
+		break;
+	case "reality":
+		return not_implemented ("reality")
+	default:
+		return not_implemented ("security " + args[Security])
+	}
+	return
+}
+
+
+func Gen_streamSettings(args URLmap) (dst *v4.StreamConfig, e error) {
+	// Set the default network to tcp and security to none
+	map_normal (args, Network, "tcp")
+	map_normal (args, Security, "none")
+	map_normal (args, TCP_HeaderType, "none")
+	dst = &v4.StreamConfig{}
+	if e = unmarshal_H (dst,
+		fmt.Sprintf (`{"network": "%s", "security": "%s"}`,
+			args[Network], args[Security],
+		)); nil != e {
+		// log
+		return
+	}
+	if e = set_stream_settings (args, dst); nil != e {
+		// log
+		return
+	}
+	return
+}
