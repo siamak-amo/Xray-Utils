6461f4477b0f7e3203a25c86da35362baf170060
siamak a.m.o <edu.siamak@gmail.com>
2025-12-09 17:46:11 +0330

    Add tests for trojan

---
+++ pkg/trojan_test.go
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+package pkg
+
+import (
+	"testing"
+)
+
+func TestGen_trojan (t *testing.T) {
+	tc := TestCase[ServerCFG] {T: t,
+		Input: map[URLMapper]string {
+	    	    Protocol:           "trojan",
+				ServerAddress:	    "vpn.net",
+				ServerPort:		    "1234",
+				Trojan_Password:    "p@ssw0rd",
+			},
+		Output: ServerCFG{},
+	}
+
+	v, e := Gen_trojan (tc.Input)
+	if nil != e {
+		t.Fatalf ("gen_trojan failed: %v\n", e)
+		return
+	}
+	tc.Do(v);
+
+	trojan := tc.Output.Settings.Servers[0];
+	tc.Assert (tc.Output.Protocol,        tc.Input[Protocol])
+	tc.Assert (trojan.Address,            tc.Input[ServerAddress])
+	tc.Assert (trojan.Port,               tc.Input[ServerPort])
+	tc.Assert (trojan.Method,             tc.Input[SS_Method])
+	tc.Assert (trojan.Password,           tc.Input[SS_Password])
+}
--- pkg/url_parser_test.go
+++ pkg/url_parser_test.go
@@ -161,6 +161,68 @@ func Test_parse_vmess_url_3 (t *testing.T) {
 	umap.Assert (t, TLS_sni,		"obdii.cfd")
 }
 
+// Simple trojan test
+func Test_parse_trojan_url_1 (t *testing.T) {
+	const TROJAN_TEST_1 = "trojan://telegram-id-privatevpns@18.203.118.212:12345?security=tls&alpn=http/1.1&headerType=none&type=tcp&sni=trojan.burgerip.co.uk"
+	umap, e := ParseURL(TROJAN_TEST_1)
+	if nil != e {
+		t.Fatalf ("parse_trojan_url failed: %v\n", e)
+	}
+
+	umap.Assert (t, Protocol,			"trojan")
+	umap.Assert (t, ServerAddress,		"18.203.118.212")
+	umap.Assert (t, ServerPort,			"12345")
+	umap.Assert (t, Trojan_Password,	"telegram-id-privatevpns")
+
+	umap.Assert (t, Network,			"tcp")
+	umap.Assert (t, TCP_HeaderType,		"none")
+
+	umap.Assert (t, Security,			"tls")
+	umap.Assert (t, TLS_sni,			"trojan.burgerip.co.uk")
+	umap.Assert (t, TLS_ALPN,			"http/1.1")
+}
+
+func Test_parse_trojan_url_2 (t *testing.T) {
+	const TROJAN_TEST_2 = "trojan://6yIitAmNwb@tel.VPNCUSTOMIZE.cam:2096?mode=gun&security=tls&alpn=h2,http/1.1&type=grpc&serviceName=@VPNCUSTOMIZE&sni=England-V2ray.vpncustomize.tech"
+	umap, e := ParseURL(TROJAN_TEST_2)
+	if nil != e {
+		t.Fatalf ("parse_trojan_url failed: %v\n", e)
+	}
+
+	umap.Assert (t, Protocol,			"trojan")
+	umap.Assert (t, ServerAddress,		"tel.VPNCUSTOMIZE.cam")
+	umap.Assert (t, ServerPort,			"2096")
+	umap.Assert (t, Trojan_Password,	"6yIitAmNwb")
+
+	umap.Assert (t, Network,			"grpc")
+	umap.Assert (t, GRPC_Mode,			"gun")
+	umap.Assert (t, GRPC_ServiceName,	"@VPNCUSTOMIZE")
+
+	umap.Assert (t, Security,			"tls")
+	umap.Assert (t, TLS_sni,			"England-V2ray.vpncustomize.tech")
+	umap.Assert (t, TLS_ALPN,			"h2,http/1.1")
+}
+
+func Test_parse_trojan_url_3 (t *testing.T) {
+	const TROJAN_TEST_3 = "trojan://XXX_VPN@104.18.13.229:2053?path=%2FTelegram%40V2ray_Alpha%2F%3Fed%3D666&security=tls&host=people-jan.xxx-java.ir&type=ws&sni=people-jan.xxx-java.ir#%F0%9F%87%A8%F0%9F%87%A6+Telegram+%40vip_vpn_2022"
+	umap, e := ParseURL(TROJAN_TEST_3)
+	if nil != e {
+		t.Fatalf ("parse_trojan_url failed: %v\n", e)
+	}
+
+	umap.Assert (t, Protocol,			"trojan")
+	umap.Assert (t, ServerAddress,		"104.18.13.229")
+	umap.Assert (t, ServerPort,			"2053")
+	umap.Assert (t, Trojan_Password,	"XXX_VPN")
+
+	umap.Assert (t, Network,			"ws")
+	umap.Assert (t, WS_Path,			"/Telegram@V2ray_Alpha/?ed=666")
+	umap.Assert (t, WS_Headers,			"people-jan.xxx-java.ir")
+
+	umap.Assert (t, Security,			"tls")
+	umap.Assert (t, TLS_sni,			"people-jan.xxx-java.ir")
+}
+
 // Simple shadowsocks test
 func Test_parse_ss_url_1 (t *testing.T) {
 	const SS_TEST_1 = "ss://YWVzLTEyOC1nY20=@104.234.168.146:16899#ConfigV2RayNG"
